d696e3188ed3f7ca7aed3c52b18ed00f96c6fa3f
SuyashIT27 <suyashvermaabes27@gmail.com>
2026-09-13 11:29:31 +0530

    adding new data

---
+++ IntegratingDatabase.py
@@ -0,0 +1,8 @@
+#SQLite is a self-contained, serverless, zero-configuration, transactional SQL database engine.
+import sqlite3
+connect=sqlite3.connect("test.db",check_same_thread=Fasle)
+cursor=connect.cursor()
+cursor.execute("""
+"""
+
+)
